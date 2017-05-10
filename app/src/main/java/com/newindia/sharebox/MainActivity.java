a44f66fd85c3202c325dd97b124b026bbceebb28
Kerr1Gan <674712964@qq.com>
2017-05-10 15:25:40 +0800

    改sdk版本为24

--- app/src/main/java/com/newindia/sharebox/MainActivity.java
+++ app/src/main/java/com/newindia/sharebox/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mToolbar= (Toolbar) findViewById(R.id.toolbar);
         mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
 
