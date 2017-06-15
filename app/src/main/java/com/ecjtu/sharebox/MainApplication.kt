cf9d9dac4ed229343edd6d9aad2587628c1c416c
xq <674712964@qq.com>
2017-06-15 18:48:21 +0800

    添加Glide 加载图片

--- app/src/main/java/com/ecjtu/sharebox/MainApplication.kt
+++ app/src/main/java/com/ecjtu/sharebox/MainApplication.kt
@@ -1,6 +1,21 @@
 package com.ecjtu.sharebox
 
 import android.app.Application
+import android.content.Context
+import android.os.Environment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory
+import com.bumptech.glide.load.DecodeFormat
+import android.os.Environment.getExternalStorageDirectory
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
+import com.bumptech.glide.load.engine.cache.LruResourceCache
+import com.bumptech.glide.load.engine.cache.MemoryCache
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.module.GlideModule
+import com.bumptech.glide.module.LibraryGlideModule
+
 
 /**
  * Created by KerriGan on 2017/6/9 0009.
@@ -13,9 +28,40 @@ class MainApplication:Application(){
     override fun onCreate() {
         super.onCreate()
 
+        var module=SimpleGlideModule()
+        var builder=GlideBuilder()
+        module.applyOptions(this,builder)
+
+        var glide=builder.build(this)
+
+        Glide.init(glide)
     }
 
-    public fun getSavedStateInstance():Map<String,Unit>{
+    fun getSavedStateInstance():Map<String,Unit>{
         return mSavedStateInstance
+    }
+
+
+
+    inner class SimpleGlideModule : AppGlideModule() {
+        override fun applyOptions(context: Context, builder: GlideBuilder) {
+            //定义缓存大小为100M
+            val diskCacheSize = 100 * 1024 * 1024
+
+            //自定义缓存 路径 和 缓存大小
+//            val diskCachePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/glideCache"
+
+            //提高图片质量
+            builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888)
+
+            //自定义磁盘缓存:这种缓存只有自己的app才能访问到
+             builder.setDiskCache(InternalCacheDiskCacheFactory( context , diskCacheSize )) ;
+            // builder.setDiskCache( new InternalCacheDiskCacheFactory( context , diskCachePath , diskCacheSize  )) ;
+            //自定义磁盘缓存：这种缓存存在SD卡上，所有的应用都可以访问到
+//            builder.setDiskCache(DiskLruCacheFactory(diskCachePath, diskCacheSize))
+
+            //Memory Cache
+            builder.setMemoryCache(LruResourceCache(24*1024*1024))
+        }
     }
 }
--- app/src/main/java/com/ecjtu/sharebox/ui/views/FileExpandableListView.java
+++ app/src/main/java/com/ecjtu/sharebox/ui/views/FileExpandableListView.java
@@ -1,16 +1,21 @@
 package com.ecjtu.sharebox.ui.views;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.AttributeSet;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.ecjtu.sharebox.R;
 import com.ecjtu.sharebox.ui.dialog.FilePickDialog;
+import com.ecjtu.sharebox.utils.fileutils.FileUtil;
 
 import java.io.File;
 import java.util.List;
@@ -19,7 +24,7 @@ import java.util.List;
  * Created by KerriGan on 2017/6/13 0013.
  */
 
-public class FileExpandableListView extends ExpandableListView{
+public class FileExpandableListView extends ExpandableListView implements View.OnClickListener{
 
     public FileExpandableListView(Context context) {
         super(context);
@@ -39,15 +44,27 @@ public class FileExpandableListView extends ExpandableListView{
 
     private ExpandableAdapter mAdapter=new ExpandableAdapter();
 
+    private LruCache<String, Bitmap> mLruCache = new LruCache<String, Bitmap>(5*1024*1024) {
+        protected int sizeOf(String key, Bitmap value) {
+            return value.getByteCount();
+        }
+    };
+
     public void initData(FilePickDialog.TabItemHolder holder){
         mTabHolder=holder;
         mFileList=mTabHolder.getFileList();
         setAdapter(mAdapter);
+        setGroupIndicator(null);
     }
 
     public void loadedData(){
         mFileList=mTabHolder.getFileList();
         ((BaseExpandableListAdapter)getExpandableListAdapter()).notifyDataSetInvalidated();
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 
 
@@ -92,9 +109,29 @@ public class FileExpandableListView extends ExpandableListView{
         @Override
         public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
             File f= (File) getGroup(groupPosition);
-            View v=LayoutInflater.from(getContext()).inflate(R.layout.layout_file_item,parent,false);
-            ((TextView)v.findViewById(R.id.text_name)).setText(f.getName());
-            return v;
+
+            if(convertView==null)
+                convertView=LayoutInflater.from(getContext()).inflate(R.layout.layout_file_item,parent,false);
+
+            ((TextView)convertView.findViewById(R.id.text_name)).setText(f.getName());
+            FileUtil.MediaFileType type=mTabHolder.getType();
+
+            ImageView icon=(ImageView) convertView.findViewById(R.id.icon);
+
+            if(type == FileUtil.MediaFileType.MOVIE ||
+                    type== FileUtil.MediaFileType.IMG){
+                Glide.with(getContext()).load(f.getAbsolutePath()).into(icon);
+            }else if(type== FileUtil.MediaFileType.APP){
+                Bitmap b= mLruCache.get(f.getAbsolutePath());
+                if(b==null){
+                    b=FileUtil.INSTANCE.getAppThumbnail(getContext(),f);
+                    mLruCache.put(f.getAbsolutePath(),b);
+                }
+                icon.setImageBitmap(b);
+            }
+
+            convertView.setOnClickListener(FileExpandableListView.this);
+            return convertView;
         }
 
         @Override
@@ -106,5 +143,7 @@ public class FileExpandableListView extends ExpandableListView{
         public boolean isChildSelectable(int groupPosition, int childPosition) {
             return true;
         }
+
+
     }
 }
