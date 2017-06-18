1bce64b21c066f7ca56e2a34434bcc0e9a051b6b
Kerr1Gan <674712964@qq.com>
2017-06-18 23:16:18 +0800

    添加后台常驻Service

---
+++ app/src/main/java/com/ecjtu/sharebox/service/MainService.java
@@ -0,0 +1,18 @@
+package com.ecjtu.sharebox.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by KerriGan on 2017/6/18.
+ */
+
+public class MainService extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
