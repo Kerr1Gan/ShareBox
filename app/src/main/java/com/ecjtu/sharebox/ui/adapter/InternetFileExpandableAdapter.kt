631dd0654975877f86c61896542b020a94f7a3b5
Kerr1Gan <674712964@qq.com>
2017-08-13 17:25:18 +0800

    dev

--- app/src/main/java/com/ecjtu/sharebox/ui/adapter/InternetFileExpandableAdapter.kt
+++ app/src/main/java/com/ecjtu/sharebox/ui/adapter/InternetFileExpandableAdapter.kt
@@ -1,53 +1,51 @@
 package com.ecjtu.sharebox.ui.adapter
 
-import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import com.ecjtu.sharebox.R
 import com.ecjtu.sharebox.ui.dialog.FilePickDialog
 import com.ecjtu.sharebox.ui.view.FileExpandableListView
 import org.ecjtu.easyserver.server.DeviceInfo
-import java.io.File
 
 /**
  * Created by KerriGan on 2017/7/16.
  */
-class InternetFileExpandableAdapter(expandableListView: FileExpandableListView):
-        FileExpandableAdapter(expandableListView){
+class InternetFileExpandableAdapter(expandableListView: FileExpandableListView) :
+        FileExpandableAdapter(expandableListView) {
 
-    private var mDeviceInfo:DeviceInfo? =null
+    private var mDeviceInfo: DeviceInfo? = null
 
-    override fun initData(holder: FilePickDialog.TabItemHolder?,oldCache:List<VH>? ) {
-        super.initData(holder,oldCache)
+    override fun initData(holder: FilePickDialog.TabItemHolder?, oldCache: List<VH>?) {
+        super.initData(holder, oldCache)
     }
 
     override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
-        var ret=super.getGroupView(groupPosition, isExpanded, convertView, parent)
-        ret.findViewById(R.id.select_all).visibility=View.INVISIBLE
+        var ret = super.getGroupView(groupPosition, isExpanded, convertView, parent)
+        ret.findViewById(R.id.select_all).visibility = View.INVISIBLE
         return ret
     }
 
     override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
-        var ret=super.getChildView(groupPosition, childPosition, isLastChild, convertView, parent)
-        ret.findViewById(R.id.check_box).visibility=View.INVISIBLE
+        var ret = super.getChildView(groupPosition, childPosition, isLastChild, convertView, parent)
+        ret.findViewById(R.id.check_box).visibility = View.INVISIBLE
         return ret
     }
 
     override fun onClick(v: View?) {
-        var tag=v?.getTag()
-        if(tag!=null && tag is String ){
-            var path=tag
-            openFile("${mDeviceInfo?.ip}:${mDeviceInfo?.port}/API/File/${path}")
+        var tag = v?.getTag()
+        if (tag != null && tag is String) {
+            var path = java.lang.String(tag)
+            openFile("${mDeviceInfo?.ip}:${mDeviceInfo?.port}/API/File/${path.hashCode()}")
             return
         }
         super.onClick(v)
     }
 
-    override fun setup(title:String) {
+    override fun setup(title: String) {
         //do nothing
     }
 
-    fun setDeviceInfo(deviceInfo:DeviceInfo){
-        mDeviceInfo=deviceInfo
+    fun setDeviceInfo(deviceInfo: DeviceInfo) {
+        mDeviceInfo = deviceInfo
     }
 }
