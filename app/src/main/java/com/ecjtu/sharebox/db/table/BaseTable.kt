1e476474fb94cc695cceac409242e5741ded7152
Ethan_Xiang <674712964@qq.com>
2017-08-15 10:18:02 +0800

    更新kotlin到1.1.3-2版本

--- app/src/main/java/com/ecjtu/sharebox/db/table/BaseTable.kt
+++ app/src/main/java/com/ecjtu/sharebox/db/table/BaseTable.kt
@@ -3,6 +3,7 @@ package com.ecjtu.sharebox.db.table
 /**
  * Created by Ethan_Xiang on 2017/8/14.
  */
-interface BaseTable{
-    val id:Int
+interface BaseTable {
+    val id: String
+        get() = "_id"
 }
