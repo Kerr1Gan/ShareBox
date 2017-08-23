ed4a7e86f1ecb45a77e55ed9a510110e59370bd0
Kerr1Gan <674712964@qq.com>
2017-08-23 22:22:27 +0800

    dev

--- easyserver/src/main/java/org/ecjtu/easyserver/server/impl/servlet/File.java
+++ easyserver/src/main/java/org/ecjtu/easyserver/server/impl/servlet/File.java
@@ -2,6 +2,7 @@ package org.ecjtu.easyserver.server.impl.servlet;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.Log;
 
 import org.ecjtu.easyserver.server.ServerManager;
 
@@ -29,7 +30,7 @@ public class File implements BaseServlet {
 
     @Override
     public void doGet(HTTPRequest httpReq, HTTPResponse res) {
-
+        Log.e("tttttttttt", "doGet");
         String uri = httpReq.getURI();
 //        try
 //        {
@@ -57,18 +58,19 @@ public class File implements BaseServlet {
             if (index >= 0) {
                 filePaths = filePaths.substring(index + 1);
             }
-
-            filePaths = filePaths.substring(0, filePaths.indexOf("."));
+            index = filePaths.indexOf(".");
+            if (index >= 0) {
+                filePaths = filePaths.substring(0, index);
+            }
         } catch (IndexOutOfBoundsException e) {
             httpReq.returnBadRequest();
             return;
         }
 
-
         try {
-            String cpy=filePaths;
+            String cpy = filePaths;
             filePaths = getFilePathByHash(Long.valueOf(filePaths));
-            if(filePaths==null) filePaths=cpy;
+            if (filePaths == null) filePaths = cpy;
         } catch (Exception e) {
             e.printStackTrace();
             httpReq.returnBadRequest();
@@ -76,7 +78,6 @@ public class File implements BaseServlet {
         }
 
         doCache(filePaths);
-
         try {
             java.io.File file = new java.io.File(filePaths);
 
@@ -122,7 +123,7 @@ public class File implements BaseServlet {
     }
 
     public static String getFilePathByHash(Long hash) {
-        List<java.io.File> fileList= ServerManager.getInstance().getSharedFileList();
+        List<java.io.File> fileList = ServerManager.getInstance().getSharedFileList();
         if (fileList == null)
             return null;
 
@@ -136,13 +137,13 @@ public class File implements BaseServlet {
     }
 
     public static Long getFileHashByPath(String path) {
-        List<java.io.File> fileList=ServerManager.getInstance().getSharedFileList();
+        List<java.io.File> fileList = ServerManager.getInstance().getSharedFileList();
 
         if (fileList == null)
             return 0L;
 
         for (int i = 0; i < fileList.size(); i++) {
-            String childPath=fileList.get(i).getPath();
+            String childPath = fileList.get(i).getPath();
             if (childPath.compareTo(path) == 0)
                 return HashUtil.BKDRHash(childPath);
         }
@@ -156,7 +157,7 @@ public class File implements BaseServlet {
 
     public static void addFiles(ArrayList<java.io.File> files) {
         boolean hasSrc = false;
-        List<java.io.File> fileList=ServerManager.getInstance().getSharedFileList();
+        List<java.io.File> fileList = ServerManager.getInstance().getSharedFileList();
         if (fileList == null || files == null)
             return;
 
@@ -179,13 +180,13 @@ public class File implements BaseServlet {
         }
     }
 
-    public void doCache(String filePaths){
-        Context context=ServerManager.getInstance().getContext();
-        Bitmap b= CacheUtil.getBitmapByCache(context,filePaths);
-        if(b==null){
-            b= ImageUtil.createVideoThumbnail(filePaths,100,100);
-            if(b!=null)
-                CacheUtil.makeCache(filePaths,b,100,100,context);
+    public void doCache(String filePaths) {
+        Context context = ServerManager.getInstance().getContext();
+        Bitmap b = CacheUtil.getBitmapByCache(context, filePaths);
+        if (b == null) {
+            b = ImageUtil.createVideoThumbnail(filePaths, 100, 100);
+            if (b != null)
+                CacheUtil.makeCache(filePaths, b, 100, 100, context);
         }
     }
 }
