4e474657099c1e2cfffe73cf6194f41a7310b19b
Kerr1Gan <674712964@qq.com>
2017-09-09 10:39:10 +0800

    dev

--- share/src/main/java/com/ecjtu/sharebox/notification/ServerComingNotification.kt
+++ share/src/main/java/com/ecjtu/sharebox/notification/ServerComingNotification.kt
@@ -4,8 +4,6 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import com.ecjtu.sharebox.ui.activity.MainActivity
-import com.ecjtu.sharebox.ui.activity.TestActivity
-import com.ecjtu.sharebox.util.activity.ActivityUtil
 
 /**
  * Created by Ethan_Xiang on 2017/9/5.
@@ -18,9 +16,9 @@ class ServerComingNotification(context: Context) : SimpleNotification(context) {
 
     fun buildServerComingNotification(title: String, content: String, ticker: String): ServerComingNotification {
         super.buildNotification(ID, title, content, ticker)
-        fullScreenIntent(getCurrentBuilder(), 0, null)
         val intent = Intent(context, MainActivity::class.java)
 //        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        fullScreenIntent(getCurrentBuilder(), 100, intent)
         getCurrentBuilder()?.setContentIntent(PendingIntent.getActivity(context, 100, intent, PendingIntent.FLAG_UPDATE_CURRENT)) //requestCode 不能为0 否则MainActivity 将重建
         return this
     }
