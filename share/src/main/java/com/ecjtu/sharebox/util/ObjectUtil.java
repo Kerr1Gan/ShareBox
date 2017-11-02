23fee525f931639f01198ef0027461756bb07853
Ethan_Xiang <674712964@qq.com>
2017-11-02 11:59:05 +0800

    dev

--- share/src/main/java/com/ecjtu/sharebox/util/ObjectUtil.java
+++ share/src/main/java/com/ecjtu/sharebox/util/ObjectUtil.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.List;
 
 /**
  * Created by Ethan_Xiang on 2017/11/1.
@@ -14,17 +13,17 @@ import java.util.List;
 public class ObjectUtil {
 
     @SuppressWarnings("unchecked")
-    public static <T> List<T> deepCopyOrThrow(List<T> src) throws IOException, ClassNotFoundException {
+    public static <T> T deepCopyOrThrow(T src) throws IOException, ClassNotFoundException {
         ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
         ObjectOutputStream out = new ObjectOutputStream(byteOut);
         out.writeObject(src);
 
         ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
         ObjectInputStream in = new ObjectInputStream(byteIn);
-        return (List<T>) in.readObject();
+        return (T) in.readObject();
     }
 
-    public static <T> List<T> deepCopy(List<T> src) {
+    public static <T> T deepCopy(T src) {
         try {
             return deepCopyOrThrow(src);
         } catch (Exception ignore) {
@@ -33,3 +32,4 @@ public class ObjectUtil {
         }
     }
 }
+
