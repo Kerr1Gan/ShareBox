4ad9d2f7e3d1e3e740b0fd05818df842d6abf999
Ethan_Xiang <Ethan_Xiang@asus.com>
2018-03-22 17:43:08 +0800

    dev

---
+++ share/src/main/java/com/ecjtu/sharebox/ui/activity/IjkVideoActivity.java
@@ -0,0 +1,20 @@
+package com.ecjtu.sharebox.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ecjtu.sharebox.R;
+
+/**
+ * Created by Ethan_Xiang on 2018/3/22.
+ */
+
+public class IjkVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ijk_video);
+    }
+}
