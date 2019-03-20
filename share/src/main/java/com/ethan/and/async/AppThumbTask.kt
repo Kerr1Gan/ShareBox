86c665eb59f1e4d3686dec3e26007b35743b4732
EthanXiang <>
2019-03-20 16:37:14 +0800

    app icon drawable width and height must be > 0

--- share/src/main/java/com/ethan/and/async/AppThumbTask.kt
+++ share/src/main/java/com/ethan/and/async/AppThumbTask.kt
@@ -11,26 +11,26 @@ import java.io.File
 /**
  * Created by KerriGan on 2017/6/18.
  */
-class AppThumbTask:AsyncTask<File,Void,Bitmap?>{
+class AppThumbTask : AsyncTask<File, Void, Bitmap?> {
 
-    private var mLruCache:LruCache<String,Bitmap>? = null
+    private var mLruCache: LruCache<String, Bitmap>? = null
 
-    private var mContext: Context? =null
+    private var mContext: Context? = null
 
-    private var mImageView:ImageView?=null
+    private var mImageView: ImageView? = null
 
-    constructor(lruCache: LruCache<String,Bitmap>,context: Context,imgView:ImageView):super(){
-        mLruCache=lruCache
-        mContext=context
-        mImageView=imgView
+    constructor(lruCache: LruCache<String, Bitmap>, context: Context, imgView: ImageView) : super() {
+        mLruCache = lruCache
+        mContext = context
+        mImageView = imgView
     }
 
     override fun doInBackground(vararg params: File?): Bitmap? {
-        val f=params[0]
+        val f = params[0]
 
         val bit = FileUtil.getAppThumbnail(mContext!!, f!!)
 
-        if(bit!=null){
+        if (bit != null) {
             mLruCache?.put(f.absolutePath, bit)
         }
 
@@ -39,16 +39,18 @@ class AppThumbTask:AsyncTask<File,Void,Bitmap?>{
 
     override fun onPostExecute(result: Bitmap?) {
         super.onPostExecute(result)
-        if(result!=null){
+        if (result != null) {
             mImageView?.setImageBitmap(result)
         }
-        mLruCache=null
-        mImageView=null
+        mLruCache = null
+        mImageView = null
+        mContext = null
     }
 
     override fun onCancelled() {
         super.onCancelled()
-        mImageView=null
-        mLruCache=null
+        mImageView = null
+        mLruCache = null
+        mContext = null
     }
 }
